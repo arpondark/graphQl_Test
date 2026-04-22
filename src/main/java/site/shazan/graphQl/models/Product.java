60c40c4dbc9bb296b3c8f200205fa47656c8aa89
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-04-22 18:38:38 +0600

    Initial commit

---
+++ src/main/java/site/shazan/graphQl/models/Product.java
@@ -0,0 +1,24 @@
+package site.shazan.graphQl.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String catagory;
+    private Double price;
+    private Integer stock;
+
+}
---
+++ src/test/java/site/shazan/graphQl/GraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package site.shazan.graphQl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
